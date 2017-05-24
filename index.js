6e6efec11e4f7ee1548be3ab4f6a933a30830874
Andy <nnlyx@hotmail.com>
2017-05-23 20:43:00 -0600

    slugify

--- index.js
+++ index.js
@@ -119,8 +119,9 @@ function Slugify(value) {
 
     let slug = value.toString()
         .toLowerCase()
+        .replace(' ','_')
         .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        //.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
